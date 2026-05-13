51c8f2f7a8ed513dd5d7ce2428c1bf05805c4ccb
Jeanierose Baril <jeanierosebaril1@gmail.com>
2026-05-13 20:31:01 +0800

    add index entry point for render

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-<?php header('Location: /brms/login.php');
+<?php
+session_start();
+require_once __DIR__ . '/includes/db.php';
+
+if (isset($_SESSION['user_id'])) {
+    header("Location: pages/dashboard.php");
+    exit;
+} else {
+    header("Location: pages/login.php");
+    exit;
+}
